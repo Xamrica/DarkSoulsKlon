b2ffb91a7ef9f201fcc5f5f839898874259e5c83
Xamrica <2miregal@gmail.com>
2021-08-05 22:34:19 +0200

    fix movement y

--- Assets/Scripts/PlayerMovementCtrl.cs
+++ Assets/Scripts/PlayerMovementCtrl.cs
@@ -28,11 +28,11 @@ public class PlayerMovementCtrl : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 v = Kamera.transform.eulerAngles;
-        Vector3 richtungsVector = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), playerRb.velocity.y, -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
+        Vector2 richtungsVector = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
         v.y = v.y - 90;
-        Vector3 richtungsVector2 = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), playerRb.velocity.y, -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
+        Vector2 richtungsVector2 = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
 
 
-        playerRb.velocity = Vector3.Normalize(richtungsVector2 * moveInputHorizontal - richtungsVector * moveInputVertical) * speed;
+        playerRb.velocity = Vector3.Normalize(new Vector3(richtungsVector2.x * moveInputHorizontal - richtungsVector.x * moveInputVertical, playerRb.velocity.y, richtungsVector2.y * moveInputHorizontal - richtungsVector.y * moveInputVertical)) * speed;
     }
 }
