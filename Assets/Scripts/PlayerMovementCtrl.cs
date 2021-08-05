e906ab7b5a18795b29faf53c9051171df9744293
Xamrica <2miregal@gmail.com>
2021-08-05 22:02:53 +0200

    updated Movement Script

--- Assets/Scripts/PlayerMovementCtrl.cs
+++ Assets/Scripts/PlayerMovementCtrl.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovementCtrl : MonoBehaviour
 {
     private Rigidbody playerRb;
+    
     public float speed;
+    public GameObject Kamera;
+
     private float moveInputHorizontal;
     private float moveInputVertical;
     // Start is called before the first frame update
@@ -24,6 +27,12 @@ public class PlayerMovementCtrl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRb.velocity = new Vector3(moveInputHorizontal * speed, playerRb.velocity.y, moveInputVertical * speed);
+        Vector3 v = Kamera.transform.eulerAngles;
+        Vector3 richtungsVector = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), playerRb.velocity.y, -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
+        v.y = v.y - 90;
+        Vector3 richtungsVector2 = new Vector3(-1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Sin(v.y * Mathf.Deg2Rad), playerRb.velocity.y, -1 * Mathf.Cos(v.x * Mathf.Deg2Rad) * Mathf.Cos(v.y * Mathf.Deg2Rad));
+
+
+        playerRb.velocity = Vector3.Normalize(richtungsVector2 * moveInputHorizontal - richtungsVector * moveInputVertical) * speed;
     }
 }
